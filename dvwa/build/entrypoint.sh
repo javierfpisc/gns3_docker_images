10ab6687da86606ab5224180097b75b13d04dc01
Javier Fernández Peón <javierfp@iessanclemente.net>
2022-03-26 02:20:19 +0100

    routes.sh

--- dvwa/build/entrypoint.sh
+++ dvwa/build/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#Si se han definido rutas se cargan
+[ -f /etc/routes/routes.sh ] && sh /etc/routes/routes.sh
+
 #Arrancamos apache y mysql
 [ -d /var/lib/mysql ] && service mariadb start
 [ -d /var/www/html ] && service apache2 start
