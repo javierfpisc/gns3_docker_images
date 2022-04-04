eb4d14112be38d75b1b30098146a499a71f485aa
Javier Fernández Peón <javierfp@iessanclemente.net>
2022-04-04 20:44:22 +0200

    webui

--- ossec/build/entrypoint.sh
+++ ossec/build/entrypoint.sh
@@ -8,5 +8,9 @@
 #Arranca ossec (server y agent)
 /etc/init.d/ossec start
 
+#Arranca mysql y apache2 (web ui server)
+[ -f /etc/init.d/apache2 ] && /etc/init.d/apache2 start
+[ -f /etc/init.d/mysql ] && /etc/init.d/mysql start
+
 exec bash
 
