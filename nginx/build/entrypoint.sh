eda00054b57482f10f61d4f55bcccf5664ba87b0
Javier Fernández Peón <javierfp@iessanclemente.net>
2022-10-20 13:10:14 +0200

    nginx

--- nginx/build/entrypoint.sh
+++ nginx/build/entrypoint.sh
@@ -4,6 +4,10 @@
 [ -f /etc/routes/routes.sh ] && sh /etc/routes/routes.sh
 [ -f /etc/iptables/rules.v4 ] && iptables-restore < /etc/iptables/rules.v4
 
+#Arrancamos SimpleHTTPServer
+cd /ser8080 && python -m SimpleHTTPServer 8080 &
+cd /ser8081 && python -m SimpleHTTPServer 8081 &
+
 #Arrancamos nginx
 [ -d /etc/nginx ] && service nginx start
 
