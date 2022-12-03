b4164630f046f886e7e021497e8d628f884cde6a
Javier Fernández Peón <javierfp@iessanclemente.net>
2022-12-03 22:51:15 +0100

    wordpress

--- wordpress/build/entrypoint.sh
+++ wordpress/build/entrypoint.sh
@@ -6,11 +6,8 @@
 
 #WORDPRESS
 #Arrancamos apache y mysql
-[ -d /var/lib/mysql ] && service mysql start
+[ -d /var/lib/mysql ] && service mariadb start
 [ -d /var/www/html ] && service apache2 start
-
-##Arrancamos fail2ban
-[ -f /etc/init.d/fail2ban ] & rm -rf /var/run/fail2ban/fail2ban.sock & service fail2ban start
 
 exec bash
 
