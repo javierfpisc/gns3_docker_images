61be966246e8495a230285f45952bb50496cfabf
Javier Fernández Peón <javierfp@iessanclemente.net>
2022-12-07 16:36:05 +0100

    symfony ok

--- symfony/build/entrypoint.sh
+++ symfony/build/entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+
+#Borramos archivos para vaciar el directorio y que el comando composer no de fallo
 rm -f /$PROJNAME/.env
 rm -f /$PROJNAME/.gitignore
 
 [ -d /$PROJNAME/src ] || composer create-project symfony/skeleton:${SYMFONYV} $PROJNAME
+
 cd /$PROJNAME
 symfony server:start
 
