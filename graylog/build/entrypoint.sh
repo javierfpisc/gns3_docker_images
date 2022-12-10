28eae39f99dce995d1e1891ea2e03c33fe477087
Javier Fernández Peón <javierfp@iessanclemente.net>
2022-12-10 17:24:02 +0100

    cambios para correr en socket tcp

--- graylog/build/entrypoint.sh
+++ graylog/build/entrypoint.sh
@@ -4,7 +4,7 @@
 [ -f /etc/routes/routes.sh ] && sh /etc/routes/routes.sh
 [ -f /etc/iptables/rules.v4 ] && iptables-restore < /etc/iptables/rules.v4
 
-cd /graylog/open-core && docker-compose up -d
+cd /docker-compose/${SERVICE} && docker-compose up -d
 
 exec bash -c "while true;do sleep 10;done"
 
