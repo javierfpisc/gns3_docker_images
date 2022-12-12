f488b995c00d8a76bc388cbcdba7fef48a7b0048
Javier Fernández Peón <javierfp@iessanclemente.net>
2022-12-12 20:50:21 +0100

    eliminado symfony

---
+++ compose-elk/build/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Si se han definido rutas e iptables se cargan
+[ -f /etc/routes/routes.sh ] && sh /etc/routes/routes.sh
+[ -f /etc/iptables/rules.v4 ] && iptables-restore < /etc/iptables/rules.v4
+
+#cd /docker-compose/${SERVICE} && docker-compose up -d
+
+exec bash
+
