9e3409ebdc9aee1a74f3c7e455a092752e7571a1
javierfp <javierfp@infol04.sanclemente.local>
2024-03-11 13:49:09 +0100

    crear ansible.cfg y hosts en arranque

--- ansible/build/entrypoint.sh
+++ ansible/build/entrypoint.sh
@@ -3,6 +3,14 @@
 #Si se han definido rutas se cargan
 [ -f /etc/routes/routes.sh ] && sh /etc/routes/routes.sh
 
+#Creación archivos ansible
+if [ ! -f /etc/ansible/ansible.cfg ]
+then
+	echo [defaults] >> /etc/ansible/ansible.cfg
+	echo inventory=/etc/ansible/hosts >> /etc/ansible/ansible.cfg
+fi
+
+[ -f /etc/ansible/hosts ] || touch /etc/ansible/hosts
 
 exec bash
 
