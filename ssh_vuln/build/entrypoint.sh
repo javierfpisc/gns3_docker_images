ff437810b48ad9d5e0d25a48586c53c9633f0e78
javierfp <javierfp@a26eql00.sanclemente.local>
2025-02-24 19:35:52 +0100

    plantilla ssh vulnerable

---
+++ ssh_vuln/build/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Si se han definido rutas e iptables se cargan
+# [ -f /etc/boot/boot.sh ] && sh /etc/boot/boot.sh
+# [ -f /etc/routes/routes.sh ] && sh /etc/routes/routes.sh
+# [ -f /etc/iptables/rules.v4 ] && iptables-restore < /etc/iptables/rules.v4
+
+/usr/sbin/sshd -D &
+
+exec bash
+
---
+++ ssh_vuln/build/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Parámetros desde variables de entorno
+SSH_VERSION=${SSH_VERSION:-"8.8p1"}  # Versión por defecto 
+
+echo "Instalando SSH versión : $SSH_VERSION"
+
+# Compilar, instalar y dependencias necesarias
+apt update && apt install -y wget vim iputils-ping iproute2 procps curl build-essential zlib1g-dev libssl-dev libpcap-dev libpam0g-dev
+
+wget https://cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-${SSH_VERSION}.tar.gz
+tar -xvzf openssh-${SSH_VERSION}.tar.gz
+cd openssh-${SSH_VERSION}
+
+./configure --prefix=/usr --sysconfdir=/etc/ssh
+make
+make install
+
+#Configuración básica y creación del usuario para el servicio
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+sed -i 's/^#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config
+useradd -r -M -d /var/empty -s /usr/sbin/nologin sshd
+
+echo "SSH $SSH_VERSION instalado."
