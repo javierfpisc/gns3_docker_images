19fb4e71f7778d1a843358cd9071f055b98ae238
javierfp <javierfp@infol05.sanclemente.local>
2025-03-06 11:37:44 +0100

    cambios entrypoint

--- elk/build/entrypoint.sh
+++ elk/build/entrypoint.sh
@@ -6,6 +6,9 @@
 #Establece los servidores DNS a 8.8.8.8
 echo "nameserver 8.8.8.8" > /etc/resolv.conf
 
+#Cambia permisos de /var/lib/elasticsearch porque a veces falla al arrancar
+chmod -R 777 /var/lib/elasticsearch
+
 #Lanza el proceso de arranque dentro de la imagen
 /usr/local/bin/start.sh &
 
