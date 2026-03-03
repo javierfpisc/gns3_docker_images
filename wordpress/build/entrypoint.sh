28c7401e7b8528b369df04c90c1208765bc4a1ab
javierfp <javierfp@iessanclemente.net>
2026-03-03 16:53:26 +0100

    permisos

--- wordpress/build/entrypoint.sh
+++ wordpress/build/entrypoint.sh
@@ -4,6 +4,11 @@
 [ -f /etc/routes/routes.sh ] && sh /etc/routes/routes.sh
 [ -f /etc/iptables/rules.v4 ] && iptables-restore < /etc/iptables/rules.v4
 
+#Ajustamos propietario
+chown -R mysql:mysql /run/mysqld
+chown -R mysql:mysql /var/lib/mysql
+chown -R www-data:www-data /var/www/html
+
 #WORDPRESS
 #Arrancamos apache y mysql
 [ -d /var/lib/mysql ] && service mariadb start
