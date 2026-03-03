bee03714fcbfd2ccadf74742040297167fa90d7f
javierfp <javierfp@iessanclemente.net>
2026-03-03 17:20:53 +0100

    permisos

--- elk/build/entrypoint.sh
+++ elk/build/entrypoint.sh
@@ -8,6 +8,10 @@ echo "nameserver 8.8.8.8" > /etc/resolv.conf
 
 #Cambia permisos de /var/lib/elasticsearch porque a veces falla al arrancar
 chmod -R 777 /var/lib/elasticsearch
+chown -R elasticsearch:elasticsearch /opt/elasticsearch
+chmod 777 /etc/elasticsearch
+chmod a+x /opt/elasticsearch/jdk/bin/java
+
 
 #Lanza el proceso de arranque dentro de la imagen
 /usr/local/bin/start.sh &
