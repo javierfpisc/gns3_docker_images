d50eaee3ea23966da8c753064c6e0dd65ac6762d
Javier Fernández Peón <javierfp@iessanclemente.net>
2026-03-04 12:57:01 +0100

    entrys cambiados

--- snort/build/entrypoint.sh
+++ snort/build/entrypoint.sh
@@ -2,8 +2,8 @@
 
 #Si se han definido rutas e iptables se cargan
 # [ -f /etc/boot/boot.sh ] && sh /etc/boot/boot.sh
-# [ -f /etc/routes/routes.sh ] && sh /etc/routes/routes.sh
-# [ -f /etc/iptables/rules.v4 ] && iptables-restore < /etc/iptables/rules.v4
+[ -f /etc/routes/routes.sh ] && sh /etc/routes/routes.sh
+[ -f /etc/iptables/rules.v4 ] && iptables-restore < /etc/iptables/rules.v4
 
 #Creación del bridge que puentea eth0 y eth1 en br0
 brctl addbr br0
@@ -13,6 +13,8 @@ ifconfig eth0 0.0.0.0
 ifconfig eth1 0.0.0.0
 ifconfig br0 up
 ifconfig br0 $IP netmask $MASK up
+#DNS
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
 
 ${SNORT_PREFIX}/bin/snort -D -c ${SNORT_PREFIX}/etc/snort/snort.lua -R ${SNORT_PREFIX}/etc/snort/rules/local.rules -i br0 -A alert_fast &
 
--- suricata/build/entrypoint.sh
+++ suricata/build/entrypoint.sh
@@ -2,8 +2,8 @@
 
 #Si se han definido rutas e iptables se cargan
 # [ -f /etc/boot/boot.sh ] && sh /etc/boot/boot.sh
-# [ -f /etc/routes/routes.sh ] && sh /etc/routes/routes.sh
-# [ -f /etc/iptables/rules.v4 ] && iptables-restore < /etc/iptables/rules.v4
+[ -f /etc/routes/routes.sh ] && sh /etc/routes/routes.sh
+[ -f /etc/iptables/rules.v4 ] && iptables-restore < /etc/iptables/rules.v4
 
 iptables -I FORWARD -j NFQUEUE --queue-num 0
 
@@ -12,6 +12,9 @@ iptables -I FORWARD -j NFQUEUE --queue-num 0
 #/usr/bin/suricata -c /etc/suricata/suricata.yaml --pidfile /var/run/suricata.pid --af-packet -D
 #Para el modo de captura nfqueue es necesario meter reglas iptables
 /usr/bin/suricata -c /etc/suricata/suricata.yaml --pidfile /var/run/suricata.pid -q 0 -D
+
+#DNS
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
 
 #Arrancamos filebeat si existe
 [ -f /etc/init.d/filebeat ] && chown root:root /etc/filebeat/filebeat.yml && /etc/init.d/filebeat start
