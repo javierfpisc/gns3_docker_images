0d83c002f7a1cce54071ff642031b0f124424e78
Javier Fernández Peón <javierfp@iessanclemente.net>
2026-03-09 20:54:41 +0100

    permisos y arranque

--- elk/build/entrypoint.sh
+++ elk/build/entrypoint.sh
@@ -6,15 +6,12 @@
 #Establece los servidores DNS a 8.8.8.8
 echo "nameserver 8.8.8.8" > /etc/resolv.conf
 
-#Cambia permisos de /var/lib/elasticsearch porque a veces falla al arrancar
-chmod -R 777 /var/lib/elasticsearch
-chown -R elasticsearch:elasticsearch /opt/elasticsearch
-chmod 777 /etc/elasticsearch
-chmod a+x /opt/elasticsearch/jdk/bin/java
-
-
 #Lanza el proceso de arranque dentro de la imagen
-/usr/local/bin/start.sh &
+#/usr/local/bin/start.sh &
+
+/etc/init.d/elasticsearch start
+/etc/init.d/logstash start
+/etc/init.d/kibana start
 
 #Uso exec para lanzar bash
 exec bash
