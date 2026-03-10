4b3cdc68cacf662c1ba6cbde20ad70ad3c312f06
Javier Fernández Peón <javierfp@iessanclemente.net>
2026-03-10 12:36:56 +0100

    cambios propietarios para docker remap

--- elk/build/entrypoint.sh
+++ elk/build/entrypoint.sh
@@ -6,6 +6,37 @@
 #Establece los servidores DNS a 8.8.8.8
 echo "nameserver 8.8.8.8" > /etc/resolv.conf
 
+#Cambio propietarios
+for dir in  /var/lib/elasticsearch /opt/elasticsearch /etc/elasticsearch /var/log/elasticsearch
+do
+    if [ -d "$dir" ]; then
+        if [ "$(stat -c %u "$dir")" != "$(id -u elasticsearch)" ]; then
+            echo "Fixing ownership of $dir"
+            chown -R elasticsearch:elasticsearch "$dir"
+        fi
+    fi
+done
+
+for dir in  /etc/logstash /opt/logstash
+do
+    if [ -d "$dir" ]; then
+        if [ "$(stat -c %u "$dir")" != "$(id -u logstash)" ]; then
+            echo "Fixing ownership of $dir"
+            chown -R logstash:logstash "$dir"
+        fi
+    fi
+done
+
+for dir in  /opt/kibana
+do
+    if [ -d "$dir" ]; then
+        if [ "$(stat -c %u "$dir")" != "$(id -u kibana)" ]; then
+            echo "Fixing ownership of $dir"
+            chown -R kibana:kibana "$dir"
+        fi
+    fi
+done
+
 #Lanza el proceso de arranque dentro de la imagen
 #/usr/local/bin/start.sh &
 
--- elk/build/install.sh
+++ elk/build/install.sh
@@ -11,13 +11,4 @@ apt update && apt install -y wget vim iputils-ping iproute2 procps curl
 sed -i s/#MAX_MAP_COUNT/MAX_MAP_COUNT/ /etc/default/elasticsearch
 echo node.store.allow_mmap: false >> /etc/elasticsearch/elasticsearch.yml
 
-#Permisos y propietario
-chown -R elasticsearch:elasticsearch /var/lib/elasticsearch
-chown -R elasticsearch:elasticsearch /opt/elasticsearch
-chown -R elasticsearch:elasticsearch /etc/elasticsearch
-chown -R elasticsearch:elasticsearch /var/log/elasticsearch
-chown -R logstash:logstash /etc/logstash/
-chown -R logstash:logstash /opt/logstash/
-chown -R kibana:kibana /opt/kibana/
-
 echo "ELK $ELK_VERSION instalado."
